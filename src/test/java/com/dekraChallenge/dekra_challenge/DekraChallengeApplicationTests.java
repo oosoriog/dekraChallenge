c2fa821b06b766368a0181c7dfb2879536703128
Oscar Osorio <oog3996@gmail.com>
2026-06-14 19:32:20 +0200

    chore(project): bootstrap Spring Boot project

---
+++ src/test/java/com/dekraChallenge/dekra_challenge/DekraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dekraChallenge.dekra_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DekraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
